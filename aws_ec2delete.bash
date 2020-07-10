778455dc9d041a7ce548d164297df6358071668d
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-10 11:59:48 +0530

    Update aws_ec2delete.bash

--- aws_ec2delete.bash
+++ aws_ec2delete.bash
@@ -1,11 +1,11 @@
-instanceId=$(aws ec2 describe-instances \
---filters Name=tag:Name,Values=testec2 Name=instance-state-code,Values=16 \
---query 'Reservations[*].Instances[*].{Instance:InstanceId}' \
---output text)
+#instanceId=$(aws ec2 describe-instances \
+#--filters Name=tag:Name,Values=testec2 Name=instance-state-code,Values=16 \
+#--query 'Reservations[*].Instances[*].{Instance:InstanceId}' \
+#--output text)
 
-aws ec2 terminate-instances --instance-id $instanceId
+#aws ec2 terminate-instances --instance-id $instanceId
 
-aws ec2 delete-key-pair --key-name myvpc-keypair
+#aws ec2 delete-key-pair --key-name myvpc-keypair
 
 vpc_id=$(aws ec2 describe-vpcs \
 --filters "Name=tag:Name,Values=Test VPC" \
