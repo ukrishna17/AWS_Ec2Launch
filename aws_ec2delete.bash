8850209c720e68a74ad92ec789c2e00edd027e30
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-10 12:22:27 +0530

    Update aws_ec2delete.bash

--- aws_ec2delete.bash
+++ aws_ec2delete.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 #instanceId=$(aws ec2 describe-instances \
 #--filters Name=tag:Name,Values=testec2 Name=instance-state-code,Values=16 \
 #--query 'Reservations[*].Instances[*].{Instance:InstanceId}' \
@@ -12,9 +13,9 @@ vpc_id=$(aws ec2 describe-vpcs \
 --query Vpcs[*].VpcId \
 --output text)
 
-for i in 'aws ec2 describe-internet-gateways --filters 'Name=attachment.vpc-id,Values=$vpc_id' | grep InternetGatewayId'
+for i in 'aws ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Values=$vpc_id | grep InternetGatewayId';\
 do 
-  aws ec2 delete-internet-gateway --internet-gateway-id=$i
+aws ec2 delete-internet-gateway --internet-gateway-id=$i 
 done
 
 
