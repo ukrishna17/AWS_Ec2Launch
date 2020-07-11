3b09ecf219f1b5996c96adbe1aca2514a7c85ae8
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-11 18:34:42 +0530

    Update aws_ec2delete.bash

--- aws_ec2delete.bash
+++ aws_ec2delete.bash
@@ -13,30 +13,6 @@ vpc_id=$(aws ec2 describe-vpcs \
 --query Vpcs[*].VpcId \
 --output text)
 
-aws ec2 describe-instances --filters "Name=tag-key,Values=testec2" --output json
-
---query 'Reservations[*].Instances[*].{Instance:InstanceId}' 
-
-aws ec2 describe-subnets --filters 'Name=vpc-id,Values=vpc-0f6d03473c2abd890' --query Subnets[*].SubnetId --output text
-aws ec2 describe-security-groups --filters 'Name=vpc-id,Values=vpc-0f6d03473c2abd890' --query SecurityGroups[*].GroupId --output text
-aws ec2 describe-egress-only-internet-gatewayss \
---filters 'Name=attachment.vpc-id,Values=vpc-0f6d03473c2abd890' \
---output text
-instanceId=$(aws ec2 describe-instances \
---filters Name=tag:Name,Values=testec2 Name=instance-state-code,Values=16 \
---query 'Reservations[*].Instances[*].{Instance:InstanceId}' \
---output text)
-
-aws ec2 delete-instance --instance-id $instanceId
-
-aws ec2 delete-key-pair --key-name myvpc-keypair
-
-
-vpc_id=$(aws ec2 describe-vpcs \
---filters "Name=tag:Name,Values=Test VPC" \
---query Vpcs[*].VpcId \
---output text)
-
 SG1=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$vpc_id" --query SecurityGroups[0].GroupId --output text)
 SG2=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$vpc_id" --query SecurityGroups[1].GroupId --output text)
 
